df984fd6cdd2981cf908878ccbdbe07dff8064d2
Dung Kim <dungnk12@gmail.com>
2019-04-02 09:29:33 +0700

    Updates

---
+++ precache-manifest.eef3610e3a3180aca4c13b265b6e5363.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f246e6a4f76e27c1c17",
+    "url": "/dectectcmnd/static/css/main.9ab4979a.chunk.css"
+  },
+  {
+    "revision": "9f246e6a4f76e27c1c17",
+    "url": "/dectectcmnd/static/js/main.e0064608.chunk.js"
+  },
+  {
+    "revision": "66bf06564f3008ffb049",
+    "url": "/dectectcmnd/static/js/runtime~main.97678dc6.js"
+  },
+  {
+    "revision": "f936883e69013787661f",
+    "url": "/dectectcmnd/static/js/2.d1638815.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dectectcmnd/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5a8ba390c14bd7cb9c9a105aa3183ffc",
+    "url": "/dectectcmnd/index.html"
+  }
+];
